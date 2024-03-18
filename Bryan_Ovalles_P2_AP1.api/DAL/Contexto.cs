a0d0dca9ea1ccfb0048104435aa8a0cda19484bd
BrynOvalles <brynovallesburgos@gmail.com>
2024-03-18 12:49:05 -0400

    creando contexto

--- Bryan_Ovalles_P2_AP1.api/DAL/Contexto.cs
+++ Bryan_Ovalles_P2_AP1.api/DAL/Contexto.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Shared.Models;
 
 namespace Bryan_Ovalles_P2_AP1.api.DAL;
 
@@ -6,4 +7,16 @@ public class Contexto : DbContext
 {
     public Contexto(DbContextOptions<Contexto> option) : base(option) { }
 
+	public DbSet<Personas> Personas { get; set; }
+	public DbSet<PersonasDetalles> PersonasDetalles { get; set; }
+	public DbSet<TiposTelefonos> TiposTelefonos { get; set; }
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		modelBuilder.Entity<TiposTelefonos>().HasData(
+			new TiposTelefonos() { TipoId = 1, Descripcion = "Teléfono" },
+			new TiposTelefonos() { TipoId = 2, Descripcion = "Celular" }
+		);
+		base.OnModelCreating(modelBuilder);
+	}
 }
