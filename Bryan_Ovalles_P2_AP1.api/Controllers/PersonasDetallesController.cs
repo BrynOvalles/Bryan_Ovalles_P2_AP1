94ba9bf112314496bbc043528d4a5e7d2fd53783
BrynOvalles <brynovallesburgos@gmail.com>
2024-03-18 12:51:01 -0400

    Creacion de los Controllers

---
+++ Bryan_Ovalles_P2_AP1.api/Controllers/PersonasDetallesController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Bryan_Ovalles_P2_AP1.api.DAL;
+using Shared.Models;
+
+namespace Bryan_Ovalles_P2_AP1.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonasDetallesController : ControllerBase
+    {
+        private readonly Contexto _context;
+
+        public PersonasDetallesController(Contexto context)
+        {
+            _context = context;
+        }
+
+        // GET: api/PersonasDetalles
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PersonasDetalles>>> GetPersonasDetalles()
+        {
+            return await _context.PersonasDetalles.ToListAsync();
+        }
+
+        // GET: api/PersonasDetalles/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PersonasDetalles>> GetPersonasDetalles(int id)
+        {
+            var personasDetalles = await _context.PersonasDetalles.FindAsync(id);
+
+            if (personasDetalles == null)
+            {
+                return NotFound();
+            }
+
+            return personasDetalles;
+        }
+
+        // POST: api/PersonasDetalles
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<PersonasDetalles>> PostPersonasDetalles(PersonasDetalles personasDetalles)
+        {
+            _context.PersonasDetalles.Add(personasDetalles);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPersonasDetalles", new { id = personasDetalles.Id }, personasDetalles);
+        }
+
+        // PUT: api/PersonasDetalles/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPersonasDetalles(int id, PersonasDetalles personasDetalles)
+        {
+            if (id != personasDetalles.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(personasDetalles).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PersonasDetallesExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/PersonasDetalles/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePersonasDetalles(int id)
+        {
+            var personasDetalles = await _context.PersonasDetalles.FindAsync(id);
+            if (personasDetalles == null)
+            {
+                return NotFound();
+            }
+
+            _context.PersonasDetalles.Remove(personasDetalles);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PersonasDetallesExists(int id)
+        {
+            return _context.PersonasDetalles.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Bryan_Ovalles_P2_AP1.api/Controllers/TiposTelefonosController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Bryan_Ovalles_P2_AP1.api.DAL;
+using Shared.Models;
+
+namespace Bryan_Ovalles_P2_AP1.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TiposTelefonosController : ControllerBase
+    {
+        private readonly Contexto _context;
+
+        public TiposTelefonosController(Contexto context)
+        {
+            _context = context;
+        }
+
+        // GET: api/TiposTelefonos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TiposTelefonos>>> GetTiposTelefonos()
+        {
+            return await _context.TiposTelefonos.ToListAsync();
+        }
+
+        // GET: api/TiposTelefonos/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TiposTelefonos>> GetTiposTelefonos(int id)
+        {
+            var tiposTelefonos = await _context.TiposTelefonos.FindAsync(id);
+
+            if (tiposTelefonos == null)
+            {
+                return NotFound();
+            }
+
+            return tiposTelefonos;
+        }
+
+        // PUT: api/TiposTelefonos/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTiposTelefonos(int id, TiposTelefonos tiposTelefonos)
+        {
+            if (id != tiposTelefonos.TipoId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(tiposTelefonos).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TiposTelefonosExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/TiposTelefonos
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<TiposTelefonos>> PostTiposTelefonos(TiposTelefonos tiposTelefonos)
+        {
+            _context.TiposTelefonos.Add(tiposTelefonos);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTiposTelefonos", new { id = tiposTelefonos.TipoId }, tiposTelefonos);
+        }
+
+        // DELETE: api/TiposTelefonos/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTiposTelefonos(int id)
+        {
+            var tiposTelefonos = await _context.TiposTelefonos.FindAsync(id);
+            if (tiposTelefonos == null)
+            {
+                return NotFound();
+            }
+
+            _context.TiposTelefonos.Remove(tiposTelefonos);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TiposTelefonosExists(int id)
+        {
+            return _context.TiposTelefonos.Any(e => e.TipoId == id);
+        }
+    }
+}
